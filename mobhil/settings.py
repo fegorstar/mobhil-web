b5fd0dcddf8bfed33cc046a1b952b181b8ba197d
Fegor Abighe-simon <oghenefegor24@gmail.com>
2023-02-21 23:17:02 +0100

    update and added car listings

--- mobhil/settings.py
+++ mobhil/settings.py
@@ -33,12 +33,16 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'cars.apps.CarsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
+    'multiselectfield',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
